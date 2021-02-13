6fa96f8203d978199d7883e6bcb77baff3c903c4
Shaun <shaun@thenetninja.co.uk>
2021-02-13 18:33:50 +0000

    lesson-3

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
   return (
-    <div>
+    <Container size="sm">
       <Typography
         variant="h6" 
         color="textSecondary"
@@ -13,19 +16,24 @@ export default function Create() {
         Create a New Note
       </Typography>
 
-      {/* <Typography
-        variant="h1" 
-        color="primary"
-        align="center"
-      >
-        Create a New Note
-      </Typography>
-      <Typography
-        noWrap
-        color="secondary"
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
-      </Typography> */}
-    </div>
+      <Button
+        onClick={() => console.log('you clicked me')}
+        type="submit" 
+        color="secondary" 
+        variant="contained">
+        Submit
+      </Button>
+
+      <br />
+
+      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+      <Button type="submit" color="default">Submit</Button>
+      
+      <ButtonGroup color="secondary" variant="contained" disableElevation>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </Container>
   )
 }
