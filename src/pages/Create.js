c53fae0f5047090cbce1c24692bcdd2492b9a642
Shaun <shaun@thenetninja.co.uk>
2021-02-13 17:39:07 +0000

    lesson-1

---
+++ src/pages/Create.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Create() {
+  return (
+    <div>
+      Create page
+    </div>
+  )
+}
---
+++ src/pages/Notes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notes() {
+  return (
+    <div>
+      Notes page
+    </div>
+  )
+}
