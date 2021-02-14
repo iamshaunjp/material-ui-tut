b0de6a6f08e7d4d6068f225595010dc0977b0a66
Shaun <shaun@thenetninja.co.uk>
2021-02-14 11:14:17 +0000

    lesson-4

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import SendIcon from '@material-ui/icons/Send'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
 
 export default function Create() {
   return (
@@ -20,20 +22,21 @@ export default function Create() {
         onClick={() => console.log('you clicked me')}
         type="submit" 
         color="secondary" 
-        variant="contained">
+        variant="contained"
+        // startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}>
         Submit
       </Button>
 
-      <br />
-
-      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
-      <Button type="submit" color="default">Submit</Button>
+      {/* icons */}
+      <br/>
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+      <AcUnitOutlinedIcon color="action" fontSize="small" />
+      <AcUnitOutlinedIcon color="error" fontSize="small" />
+      <AcUnitOutlinedIcon color="disabled" fontSize="small" />
       
-      <ButtonGroup color="secondary" variant="contained" disableElevation>
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
     </Container>
   )
 }
