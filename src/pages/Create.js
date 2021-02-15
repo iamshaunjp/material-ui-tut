4d112abb4da2a6958bfe3f6367c3841d46e527cf
Shaun <shaun@thenetninja.co.uk>
2021-02-15 13:01:30 +0000

    lesson-7

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles({
-
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
 })
 
 export default function Create() {
   const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == '') {
+      setTitleError(true)
+    }
+    if (details == '') {
+      setDetailsError(true)
+    }
+    if (title && details) {
+      console.log(title, details)
+    } 
+  }
 
   return (
     <Container size="sm">
@@ -22,15 +47,37 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
+      
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField className={classes.field}
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Title" 
+          variant="outlined" 
+          color="secondary" 
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField className={classes.field}
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
 
-      <Button
-        onClick={() => console.log('you clicked me')}
-        type="submit" 
-        color="secondary" 
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}>
-        Submit
-      </Button>
+        <Button
+          type="submit" 
+          color="secondary" 
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}>
+          Submit
+        </Button>
+      </form>
 
       
     </Container>
