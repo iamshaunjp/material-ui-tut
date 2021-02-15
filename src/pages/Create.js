8f48781053f791b0c49728a4796792dbb496138f
Shaun <shaun@thenetninja.co.uk>
2021-02-15 10:57:45 +0000

    lesson-5

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,13 +3,29 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
-import SendIcon from '@material-ui/icons/Send'
-import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      background: 'blue'
+    },
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
+
   return (
     <Container size="sm">
       <Typography
+        className={classes.title}
         variant="h6" 
         color="textSecondary"
         component="h2"
@@ -19,23 +35,15 @@ export default function Create() {
       </Typography>
 
       <Button
+        className={classes.btn}
         onClick={() => console.log('you clicked me')}
         type="submit" 
         color="secondary" 
         variant="contained"
-        // startIcon={<SendIcon />}
         endIcon={<KeyboardArrowRightIcon />}>
         Submit
       </Button>
 
-      {/* icons */}
-      <br/>
-      <AcUnitOutlinedIcon />
-      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
-      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
-      <AcUnitOutlinedIcon color="action" fontSize="small" />
-      <AcUnitOutlinedIcon color="error" fontSize="small" />
-      <AcUnitOutlinedIcon color="disabled" fontSize="small" />
       
     </Container>
   )
