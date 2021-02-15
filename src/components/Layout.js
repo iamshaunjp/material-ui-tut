f4a8d1d5d8ee72a8b385475909131384f19fc3f5
Shaun <shaun@thenetninja.co.uk>
2021-02-15 16:52:22 +0000

    lesson-13

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,49 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core'
+import Drawer from '@material-ui/core/Drawer'
+import Typography from '@material-ui/core/Typography'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
-    width: '100%'
-  }
+    width: '100%',
+  },
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
 })
 
 export default function Layout({ children }) {
   const classes = useStyles()
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app bar */}
-      <div>app bar</div>
-      
+
       {/* side drawer */}
-      <div>side drawer</div>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{ paper: classes.drawerPaper }}
+        anchor="left"
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>
+            Ninja Notes
+          </Typography>
+        </div>
+
+        {/* links/list section */}
+        
+      </Drawer>
 
       {/* main content */}
       <div className={classes.page}>
