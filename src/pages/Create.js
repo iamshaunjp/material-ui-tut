47121284acadec1f90181b971d3a889572b751bb
Borislav Stoyanov <bostoy@protonmail.com>
2021-03-31 13:22:25 +0300

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { React, Fragment } from 'react'
+import { Typography } from '@material-ui/core'
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Fragment>
+      <Typography variant="h4" align="center" component="h2" color="primary">
+        Create Something
+      </Typography>
+      <Typography color="textPrimary">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa blanditiis ea, voluptatibus nostrum fuga adipisci laudantium id a, magnam recusandae minima nesciunt corporis vitae aut! Placeat dolorem velit maiores dolorum culpa quod ipsum numquam sequi a, dicta quibusdam possimus, quasi voluptas alias consequatur. Commodi amet, quibusdam quaerat quas dolor est voluptates hic deleniti adipisci pariatur suscipit magni expedita quo odio alias molestiae tenetur ipsa! Vero aperiam cumque, ducimus perferendis distinctio nobis nihil ex, voluptatibus consequuntur voluptatem obcaecati asperiores minus sequi laudantium neque, itaque eum enim iste nostrum est? Eius, amet doloremque. Placeat expedita aliquam repudiandae molestiae dolor magni quas saepe.
+      </Typography>
+    </Fragment>
   )
 }
