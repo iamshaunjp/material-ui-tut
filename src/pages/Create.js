9625c4e121973910c2974813cf2c8970819fff2d
Borislav Stoyanov <bostoy@protonmail.com>
2021-03-31 15:06:14 +0300

    Icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,12 @@
 import { React, Fragment } from 'react'
 import { Typography, Button, Container, ButtonGroup } from '@material-ui/core'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
 
 export default function Create() {
   return (
     <Container>
       <Typography variant="h4" align="center" component="h2" color="primary">
-        Create Something
+        Create a New Note
       </Typography>
       <Typography color="textPrimary">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa blanditiis ea, voluptatibus nostrum fuga adipisci laudantium id a, magnam recusandae minima nesciunt corporis vitae aut! Placeat dolorem velit maiores dolorum culpa quod ipsum numquam sequi a, dicta quibusdam possimus, quasi voluptas alias consequatur. Commodi amet, quibusdam quaerat quas dolor est voluptates hic deleniti adipisci pariatur suscipit magni expedita quo odio alias molestiae tenetur ipsa! Vero aperiam cumque, ducimus perferendis distinctio nobis nihil ex, voluptatibus consequuntur voluptatem obcaecati asperiores minus sequi laudantium neque, itaque eum enim iste nostrum est? Eius, amet doloremque. Placeat expedita aliquam repudiandae molestiae dolor magni quas saepe.
@@ -15,16 +16,10 @@ export default function Create() {
         type="submit"
         variant="contained"
         color="secondary"
+        endIcon={<ArrowForwardIosIcon fontSize="small" />}
       >
         Submit
         </Button>
-
-      <ButtonGroup variant="contained" color="primary">
-        <Button color="secondary" variant="outlined" >1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-
-      </ButtonGroup>
     </Container >
   )
 }
