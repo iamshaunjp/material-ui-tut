3b1c151a714782cd8fd5682393d0c3de58cac0b3
vishalicious213 <vishalicious213@gmail.com>
2021-05-02 19:22:13 -0700

    completed tutorial #3 - buttons. also used Container

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+// import { ButtonGroup } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant='h6'
         component='h2'
@@ -27,6 +30,26 @@ export default function Create() {
       >
         Officia proident eu in deserunt pariatur non officia sit. Sunt non mollit sit deserunt cupidatat commodo amet. Do consectetur sit do id aliquip voluptate cillum id. Sunt commodo cillum consectetur ad excepteur do adipisicing reprehenderit sit elit fugiat tempor esse. Nulla dolore eu laborum ex laborum est commodo reprehenderit. Elit deserunt non voluptate magna deserunt id sint. Consequat non anim eiusmod dolor exercitation culpa aliqua ex elit voluptate voluptate magna.
     </Typography> */}
-    </div>
+
+    <Button 
+      type='submit' 
+      color='secondary' 
+      variant='contained' 
+      // disableElevation
+      onClick={() => console.log('Submit button clicked')}
+    >
+      Submit
+    </Button>
+
+    {/* <Button type='submit' color='primary'>Submit</Button>
+    <Button type='submit' color='secondary'>Submit</Button>
+    <Button type='submit' color='secondary' variant='outlined' >Submit</Button>
+
+    <ButtonGroup color='secondary' variant='contained'>
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+    </ButtonGroup> */}
+    </Container>
   )
 }
