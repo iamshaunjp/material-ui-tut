e5827e06da9abe1eee897ee91e56914f6184d7f3
vishalicious213 <vishalicious213@gmail.com>
2021-05-02 18:36:24 -0700

    completed tutorial #2 - typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant='h6'
+        component='h2'
+        color='textSecondary'
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      {/* <Typography 
+        variant='h1' 
+        color='primary'
+        align='center'
+      >
+        Create a New Note
+    </Typography>
+
+    <Typography 
+        color='secondary'
+        noWrap
+      >
+        Officia proident eu in deserunt pariatur non officia sit. Sunt non mollit sit deserunt cupidatat commodo amet. Do consectetur sit do id aliquip voluptate cillum id. Sunt commodo cillum consectetur ad excepteur do adipisicing reprehenderit sit elit fugiat tempor esse. Nulla dolore eu laborum ex laborum est commodo reprehenderit. Elit deserunt non voluptate magna deserunt id sint. Consequat non anim eiusmod dolor exercitation culpa aliqua ex elit voluptate voluptate magna.
+    </Typography> */}
     </div>
   )
 }
