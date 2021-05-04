6803f9e068d68f4f0271c48b5d6d4c8d6ac3d9a6
vishalicious213 <vishalicious213@gmail.com>
2021-05-03 20:30:36 -0700

    completed tutorial #10 - Grid System (Grid container, Grid item, Paper)

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { Container } from '@material-ui/core'
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
-  
+
   useEffect(() => {
     fetch('http://localhost:8000/notes')
       .then(res => res.json())
@@ -10,10 +13,29 @@ export default function Notes() {
   }, [])
 
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
-    </div>
+    <Container>
+      {/* <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+
+      <Grid container>
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{ note.title }</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
