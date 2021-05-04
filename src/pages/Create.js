0c5c9fbaa5341e822aea23819bcbce554d4e7263
vishalicious213 <vishalicious213@gmail.com>
2021-05-03 18:17:32 -0700

    completed tutorial #9 - using JSON server (built Notes, added db.json, installed json-server, ran it, updated Create to save to file, added item to db.json)

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -13,6 +13,7 @@ import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
+import { useHistory } from 'react-router'
 
 const useStyles = makeStyles({
   btn: {
@@ -35,6 +36,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles()
+  const history = useHistory()
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
   const [titleError, setTitleError] = useState(false)
@@ -55,7 +57,12 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(title, details, category)
+      // console.log(title, details, category)
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'))
     }
   }
 
