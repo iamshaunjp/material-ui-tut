b1002d1d0b66ade91ec70473e8c7d26b7491cdff
vishalicious213 <vishalicious213@gmail.com>
2021-05-04 16:55:10 -0700

    completed tutorial #13 - Permanent Drawer (Drawer, permanent variant, style for container, drawer, paper in drawer and page which affects all children)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,46 @@
 import { makeStyles } from '@material-ui/core'
 import React from 'react'
+import Drawer from '@material-ui/core/Drawer'
+import Typography from '@material-ui/core/Typography'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
     page: {
         background: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: 'flex'
     }
 })
 
 export default function Layout({ children }) {
     const classes = useStyles()
     return (
-        <div>
+        <div className={classes.root}>
             {/* app bar */}
 
             {/* side drawer */}
-            
+            <Drawer
+                className={classes.drawer}
+                variant='permanent'
+                anchor='left'
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div>
+                    <Typography variant='h5'>
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
+
             <div className={classes.page}>
                 {children}
             </div>
