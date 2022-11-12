4572f1f059fd7239d9fd07c1547146c96b2d790c
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-11-12 11:25:31 +0100

    lesson7

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import { Typography, Button, Container } from '@mui/material'
+import React, { useState } from 'react'
+import { Typography, Button, Container, TextField } from '@mui/material'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles({
-
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
 })
 
 export default function Create() {
   const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == '') {
+      setTitleError(true)
+    }
+    if (details == '') {
+      setDetailsError(true)
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  }
 
   return (
     <Container size="sm">
@@ -21,14 +46,44 @@ export default function Create() {
         Create a New Note
       </Typography>
 
-      <Button
-        onClick={() => console.log('you clicked me')}
-        type="submit" 
-        color="secondary" 
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}>
-        Submit
-      </Button>
+      <form 
+        noValidate 
+        autoComplete='off'
+        onSubmit={handleSubmit}
+      >
+        <TextField 
+          onChange={e => setTitle(e.target.value)}
+          className={classes.field}
+          label='Note Title'
+          variant='outlined'
+          color='secondary'
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField 
+          onChange={e => setDetails(e.target.value)}
+          className={classes.field}
+          label='Details'
+          variant='outlined'
+          color='secondary'
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+
+        <Button
+          type="submit" 
+          color="secondary" 
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}>
+          Submit
+        </Button>
+      </form>
+
+      
 
       
     </Container>
