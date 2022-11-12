9c0f5bfb502b6af6b52dc06f80514f8276595387
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-11-12 21:19:24 +0100

    adding navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,71 @@
 import { ClassNames } from "@emotion/react"
+import { AddCircleOutlined, SubjectOutlined } from "@mui/icons-material"
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
 import { makeStyles } from "@mui/styles"
+import { useHistory } from "react-router-dom"
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
   page: {
-    background: '#e9e9e9',
+    background: '#f9f9f9',
     width: '100%'
+  },
+  drawer: {
+    width: drawerWidth
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  root: {
+    display: 'flex'
   }
 })
 
 export const Layout = ({children}) => {
+  const history = useHistory()
   const classes = useStyles()
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlined color='secondary'/>,
+      path: '/'
+    },
+    {
+      text: 'Create Note',
+      icon: <AddCircleOutlined color='secondary'/>,
+      path: '/create'
+    }
+  ]
+
   return (
-    <div>
-      Layout
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        anchor='left'
+        classes={{ paper: classes.drawerPaper}}
+      >
+        <div>
+          <Typography variant='h5'>
+            Ninja Notes
+          </Typography>
+        </div>
+
+        <List>
+          {menuItems.map(item => (
+            <ListItem 
+              key={item.text}
+              button
+              onClick={() => history.push(item.path)}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text}/>
+            </ListItem>
+          ))}
+        </List>
+
+      </Drawer>
       <div className={classes.page}>
         {children}
       </div>
